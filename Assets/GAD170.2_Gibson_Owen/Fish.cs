973e600f1d2fee9936695e5b39f65131e866f846
owengibson <114120616+owengibson@users.noreply.github.com>
2022-10-13 18:56:43 +1100

    Created Fish.cs

---
+++ Assets/GAD170.2_Gibson_Owen/Fish.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public void SetInitialState(string newSpecies, float newLength, int newPrice)
+    {
+        
+    }
+}
